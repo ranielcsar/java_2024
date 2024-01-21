ce48fad9f64c72e2968f72de378501b736031eac
ranoob <whyfck@gmail.com>
2024-01-21 14:03:37 -0300

    refac: change Student list to Object

--- students1/models/StudentsVector.java
+++ students1/models/StudentsVector.java
@@ -1,45 +1,61 @@
 package models;
 
 public class StudentsVector {
-  private Student[] students;
+  private Object[] objects;
   private int listSize = 0;
 
   public StudentsVector(int vectorSize) {
-    students = new Student[vectorSize];
+    objects = new Object[vectorSize];
   }
 
   public void add(Student student) {
-    students[listSize] = student;
+    handleListSpace();
+
+    objects[listSize] = student;
     listSize++;
   }
 
   private boolean occupiedPosition(int position) {
-    if (position < students.length && position >= 0) {
+    if (position < objects.length && position >= 0) {
       return true;
     }
 
     return false;
   }
 
+  private void handleListSpace() {
+    if (listSize == objects.length) {
+      Object[] newList = new Object[objects.length * 2];
+
+      for (int i = 0; i < objects.length; i++) {
+        newList[i] = objects[i];
+      }
+
+      objects = newList;
+    }
+  }
+
   public void addAt(int index, Student student) {
+    handleListSpace();
+
     if (!occupiedPosition(index)) {
       throw new IllegalArgumentException("Posição inválida");
     }
 
     for (int i = listSize - 1; i >= index; i--) {
-      students[i + 1] = students[i];
+      objects[i + 1] = objects[i];
     }
 
-    students[index] = student;
+    objects[index] = student;
     listSize++;
   }
 
-  public Student get(int index) {
+  public Object get(int index) {
     if (!occupiedPosition(index)) {
       throw new IllegalArgumentException("Posição inválida");
     }
 
-    return students[index];
+    return objects[index];
   }
 
   public void remove(int index) {
@@ -48,7 +64,7 @@ public class StudentsVector {
     }
 
     for (int i = index; i < listSize - 1; i++) {
-      students[i] = students[i + 1];
+      objects[i] = objects[i + 1];
     }
 
     listSize--;
@@ -56,7 +72,7 @@ public class StudentsVector {
 
   public boolean contain(Student student) {
     for (int i = 0; i < listSize; i++) {
-      if (student == students[i]) {
+      if (student == objects[i]) {
         return true;
       }
     }
@@ -77,11 +93,11 @@ public class StudentsVector {
     builder.append("[");
 
     for (int i = 0; i < listSize - 1; i++) {
-      builder.append(students[i]);
+      builder.append(objects[i]);
       builder.append(", ");
     }
 
-    builder.append(students[listSize - 1]);
+    builder.append(objects[listSize - 1]);
     builder.append("]");
 
     return builder.toString();
